618578caae93a4783e921f03d8422b1354db62bb
dexter <mhlee1116@gmail.com>
2015-05-07 15:05:15 -0400

    Add Bayesian

--- src/cmu/ml/pgm/project/SyntheticTest3Relations.java
+++ src/cmu/ml/pgm/project/SyntheticTest3Relations.java
@@ -21,67 +21,135 @@ public class SyntheticTest3Relations {
 				"Data/synthetic/R.2.3.test.dat", false);
 
 		System.out.println("starting");
-		CollectiveMatrixFactorizationResult featuresResult = CollectiveMatrixFactorization
-				.factorizeMatricesWithFeatures(mfTrain, latentDim,
-						maxIterOuter, maxIterInner, step, step);
-		System.out.println("done");
-		for (int k = 0; k < featuresResult.getNumIntermediate(); k++) {
-			int s = 0, t = 1;
-			Matrix rFeatures = featuresResult.getIntermediateRelations(k, s, t);
-			Matrix testR = mfTest.getRelations(s, t);
-			double featuresError = 0;
-			int nTest = 0;
-			for (int i = 0; i < mfTest.getNumItems(s); i++)
-				for (int j = 0; j < mfTest.getNumItems(t); j++)
-					if (testR.get(i, j) != 0) {
-						nTest++;
-						featuresError += Math.pow(
-								testR.get(i, j) - rFeatures.get(i, j), 2);
-					}
-			featuresError = Math.sqrt(featuresError / nTest);
-			System.out.printf("RMSE of R" + (s + 1) + (t + 1)
-					+ " with features = %f\n", featuresError);
+		boolean isFeatures = false;
+		boolean isBayesian = true;
+		if(isFeatures) {
+			CollectiveMatrixFactorizationResult featuresResult = CollectiveMatrixFactorization
+					.factorizeMatricesWithFeatures(mfTrain, latentDim,
+							maxIterOuter, maxIterInner, step, step);
+			System.out.println("done");
+			for (int k = 0; k < featuresResult.getNumIntermediate(); k++) {
+				int s = 0, t = 1;
+				Matrix rFeatures = featuresResult.getIntermediateRelations(k, s, t);
+				Matrix testR = mfTest.getRelations(s, t);
+				double featuresError = 0;
+				int nTest = 0;
+				for (int i = 0; i < mfTest.getNumItems(s); i++)
+					for (int j = 0; j < mfTest.getNumItems(t); j++)
+						if (testR.get(i, j) != 0) {
+							nTest++;
+							featuresError += Math.pow(
+									testR.get(i, j) - rFeatures.get(i, j), 2);
+						}
+				featuresError = Math.sqrt(featuresError / nTest);
+				System.out.printf("RMSE of R" + (s + 1) + (t + 1)
+						+ " with features = %f\n", featuresError);
 
+			}
+			System.out.println();
+
+			for (int k = 0; k < featuresResult.getNumIntermediate(); k++) {
+				int s = 0, t = 2;
+				Matrix rFeatures = featuresResult.getIntermediateRelations(k, s, t);
+				Matrix testR = mfTest.getRelations(s, t);
+				double featuresError = 0;
+				int nTest = 0;
+				for (int i = 0; i < mfTest.getNumItems(s); i++)
+					for (int j = 0; j < mfTest.getNumItems(t); j++)
+						if (testR.get(i, j) != 0) {
+							nTest++;
+							featuresError += Math.pow(
+									testR.get(i, j) - rFeatures.get(i, j), 2);
+						}
+				featuresError = Math.sqrt(featuresError / nTest);
+				System.out.printf("RMSE of R" + (s + 1) + (t + 1)
+						+ " with features = %f\n", featuresError);
+
+			}
+			System.out.println();
+
+			for (int k = 0; k < featuresResult.getNumIntermediate(); k++) {
+				int s = 1, t = 2;
+				Matrix rFeatures = featuresResult.getIntermediateRelations(k, s, t);
+				Matrix testR = mfTest.getRelations(s, t);
+				double featuresError = 0;
+				int nTest = 0;
+				for (int i = 0; i < mfTest.getNumItems(s); i++)
+					for (int j = 0; j < mfTest.getNumItems(t); j++)
+						if (testR.get(i, j) != 0) {
+							nTest++;
+							featuresError += Math.pow(
+									testR.get(i, j) - rFeatures.get(i, j), 2);
+						}
+				featuresError = Math.sqrt(featuresError / nTest);
+				System.out.printf("RMSE of R" + (s + 1) + (t + 1)
+						+ " with features = %f\n", featuresError);
+
+			}
 		}
-		System.out.println();
 
-		for (int k = 0; k < featuresResult.getNumIntermediate(); k++) {
-			int s = 0, t = 2;
-			Matrix rFeatures = featuresResult.getIntermediateRelations(k, s, t);
-			Matrix testR = mfTest.getRelations(s, t);
-			double featuresError = 0;
-			int nTest = 0;
-			for (int i = 0; i < mfTest.getNumItems(s); i++)
-				for (int j = 0; j < mfTest.getNumItems(t); j++)
-					if (testR.get(i, j) != 0) {
-						nTest++;
-						featuresError += Math.pow(
-								testR.get(i, j) - rFeatures.get(i, j), 2);
-					}
-			featuresError = Math.sqrt(featuresError / nTest);
-			System.out.printf("RMSE of R" + (s + 1) + (t + 1)
-					+ " with features = %f\n", featuresError);
+		if(isBayesian) {
+			CollectiveMatrixFactorizationResult featuresResult = BayesianMatrixFactorization
+					.factorizeMatrixWithFeatures(mfTrain, latentDim, 20, 1);
+			System.out.println("done");
+			for (int k = 0; k < featuresResult.getNumIntermediate(); k++) {
+				int s = 0, t = 1;
+				Matrix rFeatures = featuresResult.getIntermediateRelations(k, s, t);
+				Matrix testR = mfTest.getRelations(s, t);
+				double featuresError = 0;
+				int nTest = 0;
+				for (int i = 0; i < mfTest.getNumItems(s); i++)
+					for (int j = 0; j < mfTest.getNumItems(t); j++)
+						if (testR.get(i, j) != 0) {
+							nTest++;
+							featuresError += Math.pow(
+									testR.get(i, j) - rFeatures.get(i, j), 2);
+						}
+				featuresError = Math.sqrt(featuresError / nTest);
+				System.out.printf("RMSE of R" + (s + 1) + (t + 1)
+						+ " with features = %f\n", featuresError);
 
-		}
-		System.out.println();
+			}
+			System.out.println();
 
-		for (int k = 0; k < featuresResult.getNumIntermediate(); k++) {
-			int s = 1, t = 2;
-			Matrix rFeatures = featuresResult.getIntermediateRelations(k, s, t);
-			Matrix testR = mfTest.getRelations(s, t);
-			double featuresError = 0;
-			int nTest = 0;
-			for (int i = 0; i < mfTest.getNumItems(s); i++)
-				for (int j = 0; j < mfTest.getNumItems(t); j++)
-					if (testR.get(i, j) != 0) {
-						nTest++;
-						featuresError += Math.pow(
-								testR.get(i, j) - rFeatures.get(i, j), 2);
-					}
-			featuresError = Math.sqrt(featuresError / nTest);
-			System.out.printf("RMSE of R" + (s + 1) + (t + 1)
-					+ " with features = %f\n", featuresError);
+			for (int k = 0; k < featuresResult.getNumIntermediate(); k++) {
+				int s = 0, t = 2;
+				Matrix rFeatures = featuresResult.getIntermediateRelations(k, s, t);
+				Matrix testR = mfTest.getRelations(s, t);
+				double featuresError = 0;
+				int nTest = 0;
+				for (int i = 0; i < mfTest.getNumItems(s); i++)
+					for (int j = 0; j < mfTest.getNumItems(t); j++)
+						if (testR.get(i, j) != 0) {
+							nTest++;
+							featuresError += Math.pow(
+									testR.get(i, j) - rFeatures.get(i, j), 2);
+						}
+				featuresError = Math.sqrt(featuresError / nTest);
+				System.out.printf("RMSE of R" + (s + 1) + (t + 1)
+						+ " with features = %f\n", featuresError);
 
+			}
+			System.out.println();
+
+			for (int k = 0; k < featuresResult.getNumIntermediate(); k++) {
+				int s = 1, t = 2;
+				Matrix rFeatures = featuresResult.getIntermediateRelations(k, s, t);
+				Matrix testR = mfTest.getRelations(s, t);
+				double featuresError = 0;
+				int nTest = 0;
+				for (int i = 0; i < mfTest.getNumItems(s); i++)
+					for (int j = 0; j < mfTest.getNumItems(t); j++)
+						if (testR.get(i, j) != 0) {
+							nTest++;
+							featuresError += Math.pow(
+									testR.get(i, j) - rFeatures.get(i, j), 2);
+						}
+				featuresError = Math.sqrt(featuresError / nTest);
+				System.out.printf("RMSE of R" + (s + 1) + (t + 1)
+						+ " with features = %f\n", featuresError);
+
+			}
 		}
 	}
 
